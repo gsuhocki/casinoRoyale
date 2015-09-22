2a91ee43f6642d03e9ca3b6cde74814732d5f472
Sean Strauss <sstrauss@Zips-MacBook-Pro-4.local>
2015-09-22 16:27:52 -0400

    Worked on dealer class and Test and finished Player class

--- src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Dealer.java
@@ -4,4 +4,53 @@ package io.illcoder.casinoRoyale.core;
  * Created by syoung on 9/22/15.
  */
 public class Dealer {
+    private int wager;
+
+   public String compareHands(){
+       return "Player wins";
+   }
+
+    /**
+     * Dealer constructor.
+     */
+    public Dealer(){
+
+    }
+
+    public Deck createDeck(){
+        return new Deck();
+    }
+
+    /**
+     * Dealer takes wager from player
+     * @param wager how much money player is betting
+     * @return Confirmation string.
+     */
+    public String takeWager(int wager){
+       String wagerConfirm = "We have accepted your bet of " + wager;
+        return wagerConfirm;
+    }
+
+    /**
+     * Starts the game and
+     * @return Message to user letting them know which game has started.
+     */
+    public String runGame(){
+        String gameMessage= "Starting game... ";
+        return gameMessage;
+    }
+
+    /**
+     * Deals hand to the player and CPU and
+     * @return a String with message saying you got your hand.
+     */
+    public String dealHand(){
+        String dealHandMessage = "That's your hand, deal with it!";
+        return dealHandMessage;
+    }
+
+    public String quit(){
+        return "Returning to main menu";
+    }
+
 }
--- src/main/java/io/illcoder/casinoRoyale/core/Player.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Player.java
@@ -4,4 +4,12 @@ package io.illcoder.casinoRoyale.core;
  * Created by syoung on 9/22/15.
  */
 public class Player {
+    private String name;
+    private int money = 500;
+
+
+    public Player(String name){
+        this.name = name;
+    }
+
 }
