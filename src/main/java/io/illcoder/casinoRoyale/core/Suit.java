ddb09788e47adc5180597f04bebe881ed7cf89ca
Gregory Suhocki <gsuhocki@Zips-MacBook-Pro-11.local>
2015-09-23 16:18:12 -0400

    added comment to suit enum

--- src/main/java/io/illcoder/casinoRoyale/core/Suit.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Suit.java
@@ -2,7 +2,10 @@ package io.illcoder.casinoRoyale.core;
 
 /**
  * Created by gsuhocki on 9/23/15.
+ * Add all suits for possible card game hands to the casino
  */
+
+
 public enum Suit {
     SPADES, HEARTS, CLUBS, DIAMONDS;
 }
