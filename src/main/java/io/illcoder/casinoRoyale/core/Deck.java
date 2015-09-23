f83e9caf7263e33a20c27947f67690d32fe2b356
shy1 <sethyoungzip@gmail.com>
2015-09-23 18:39:44 -0400

    cleaned up feature_03 (deck) branch

---
+++ src/main/java/io/illcoder/casinoRoyale/core/Deck.java
@@ -0,0 +1,24 @@
+package io.illcoder.casinoRoyale.core;
+import java.util.*;
+
+/**
+ * Created by syoung on 9/22/15.
+ * Build card deck to play poker or blackjack
+ */
+public class Deck {
+
+    public static Card cards[];
+
+    public Deck() {
+        this.cards = new Card[52];
+        for (i=0;i<cards.length;i++){
+            Card card = new Card();
+            this.cards[i] = card;
+        }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+
+    }
+
+}
