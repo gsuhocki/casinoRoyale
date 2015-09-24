a80d8fc271e3cbc7b232c8b04f8b24a8ba3436d5
Sean Strauss <sstrauss@Zips-MacBook-Pro-4.local>
2015-09-24 10:22:55 -0400

    Removed everything except blackjack logic

--- src/main/java/io/illcoder/casinoRoyale/poker/PokerLogic.java
+++ src/main/java/io/illcoder/casinoRoyale/poker/PokerLogic.java
@@ -1,9 +1,7 @@
 package io.illcoder.casinoRoyale.poker;
 
-import io.illcoder.casinoRoyale.core.CardGameLogic;
-
 /**
  * Created by syoung on 9/22/15.
  */
-public class PokerLogic extends CardGameLogic {
+public class PokerLogic {
 }
