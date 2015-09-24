2988f4afb7d7ad88be0aa22d0afdfda9fb20124f
Gregory Suhocki <gsuhocki@Zips-MacBook-Pro-3.local>
2015-09-24 12:16:57 -0400

    updates made to deck

--- src/main/java/io/illcoder/casinoRoyale/core/Deck.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Deck.java
@@ -1,20 +1,28 @@
 package io.illcoder.casinoRoyale.core;
 
+import java.util.*;
 /**
  * Created by syoung on 9/22/15.
+ *Build card deck to play poker or blackjack
  */
 public class Deck {
 
-    public static Card[] cards = new Card;
 
-    public Deck() {
-        int i = 0;
-            for (Suit suit: Suit.values());
-                for (Rank rank : Rank.values());
-                    cards[i++] = new Card[];
+        public static Card cards[];
+
+        public Deck() {
+            this.cards = new Card[52];
+            for (int i = 0; i < cards.length; i++) {
+                Card card = new Card();
+                this.cards[i] = card;
+            }
+        }
 
 
     }
 
-}
+
+
+
+
 
---
+++ src/test/java/test/illcoder/casinoRoyale/testDeck.java
@@ -0,0 +1,22 @@
+package test.illcoder.casinoRoyale;
+
+import io.illcoder.casinoRoyale.core.Deck;
+import io.illcoder.casinoRoyale.core.Suit;
+import org.junit.Test;
+
+/**
+ * Created by gsuhocki on 9/24/15.
+ */
+public class testDeck {
+
+
+    Deck deck = new Deck();
+
+        @Test
+        public void testIfCardIsBeingBuilt(){
+            System.out.println(Suit.HEARTS);
+        }
+
+
+
+}
