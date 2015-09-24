dcecf90433cae5eb05fda5e278ffc7655717f2b1
Gregory Suhocki <gsuhocki@Zips-MacBook-Pro-3.local>
2015-09-24 16:24:28 -0400

    deck ready for review

--- src/main/java/io/illcoder/casinoRoyale/core/Deck.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Deck.java
@@ -1,25 +1,60 @@
 package io.illcoder.casinoRoyale.core;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.*;
+
 /**
  * Created by syoung on 9/22/15.
  *Build card deck to play poker or blackjack
+ *
  */
 public class Deck {
+    List<Card> deck;
 
-
-        public static Card cards[];
-
-        public Deck() {
-            this.cards = new Card[52];
-            for (int i = 0; i < cards.length; i++) {
-                Card card = new Card();
-                this.cards[i] = card;
+    /**
+     *Create a new array list for each Suit of cards 2 through Ace and prints out the size of the deck
+     * The deck is the shuffled after created
+     */
+    public Deck(){
+        deck = new ArrayList();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+               deck.add(new Card(suit, rank));
             }
         }
-
-
+       // System.out.println(deck.get(0));
+        //Collections.shuffle(deck);
+        //System.out.println(deck.get(0));
     }
+
+
+    public void shuffleDeck(List<Card> _deck){
+        //private List<Card> sDeck;
+       // sDeck = _deck;
+        Collections.shuffle(_deck);
+        //return sDeck;
+    }
+
+    public List<Card> getShuffledDeck(){
+        return deck;
+    }
+
+    public static void main (String[] args){
+        Deck deck1 = new Deck();
+        List<Card> deck2 = deck1.getShuffledDeck();
+        System.out.println(deck2.get(0));
+        deck1.shuffleDeck(deck2);
+
+        System.out.println(deck2.get(0));
+    }
+
+}
+
+
+
+
+
+
 
 
 
--- src/main/java/io/illcoder/casinoRoyale/core/Rank.java
+++ src/main/java/io/illcoder/casinoRoyale/core/Rank.java
@@ -1,38 +1,14 @@
 package io.illcoder.casinoRoyale.core;
 
 /**
-<<<<<<< HEAD
- * Created by gsuhocki on 9/23/15.
-=======
  * Created by clouie on 9/22/15.
  * Adds all rank and numeric values for both poker and black jack for all possible cards
->>>>>>> master
  */
 public enum Rank {
     TWO(2,2), THREE(3,3), FOUR(4,4), FIVE(5,5), SIX(6,6), SEVEN(7,7), EIGHT(8,8), NINE(9,9), TEN(10,10), JACK(11,10),
     QUEEN(12,10), KING(13,10), ACE(14,11);
 
     private int pokerValue;
-<<<<<<< HEAD
-    private int blackJackValue;
-
-    Rank(int pokerValue, int blackJackValue){
-        this.pokerValue = pokerValue;
-        this.blackJackValue = blackJackValue;
-    }
-
-    /**
-     * these functions get the various attributes for the Rank Object
-     */
-    public int getPokerValue(){
-        return this.pokerValue;
-        }
-
-    public int getBlackJackValue(){
-        return this.blackJackValue;
-        }
-
-=======
     private int blackjackValue;
 
     /**
@@ -61,5 +37,4 @@ public enum Rank {
     public int getBlackjackValue(){
         return this.blackjackValue;
     }
->>>>>>> master
 }
